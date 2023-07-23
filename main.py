c6c4ec039c26f9e181944d2f1eb292a9ea68c57f
Himaanii <87857620+Himaanii@users.noreply.github.com>
2023-07-23 17:10:42 +0100

    Efficiency Improvement
    I made the code more adaptable for longer inputs and simpler using a for loop.
    
    I used the .reverse() method to make the if statement shorter and easier to write and read.
    
    I also fixed the bug, so now the program takes into account inputs with upper and lower case letters

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 print("Enter a 3 letter word.")
-a =[]
-word = input()
-a.append (word [:1])  
-a.append (word [1:2])
-a.append (word [2:3])
-print(a[0])
-print(a[1])
-print(a[2])
-if a[2] == a[0] and a[1] == a[1] and a[0] == a[2]:
-  print("Well done!", word, "is a pallindrome.")
+a = []
+r = []
+
+word = input() 
+
+for i in range(3):
+  a.append(word[i].upper())  
+  r.append(word[i].upper())
+
+#print(a)
+#print(r)
+  
+r.reverse()
+
+if a==r:
+  print(word, "is a palindrome.")
 else:
-  print("Sorry", word, "isn't a pallindrome.")
+  print("Sorry", word, "is not a palindrome")
+ 
